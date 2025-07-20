a52a724ebba449f828aff33092a7f5cddfed1a58
Shane Williamson <67734801+wert551@users.noreply.github.com>
2025-07-20 00:36:40 -0700

    Update toggle_media.sh

--- toggle_media.sh
+++ toggle_media.sh
@@ -3,7 +3,14 @@
 export DISPLAY=:0
 
 CARPLAY_CMD="/home/pi/react-carplay/start.sh"
-MEDIA_CMD="celluloid /media/pi/DASHCAM/DCIM"
+
+DASHCAM_DIR="/media/pi/DASHCAM/DCIM"
+
+if [ -d "$DASHCAM_DIR" ]; then
+  MEDIA_CMD="celluloid \"$DASHCAM_DIR\""
+else
+  MEDIA_CMD="celluloid --fullscreen"
+fi
 
 kill_if_running() {
   pgrep -f "$1" && pkill -f "$1"
@@ -13,9 +20,9 @@ if pgrep -f react-carplay > /dev/null; then
   # CarPlay is running → switch to media player
   kill_if_running react-carplay
   sleep 1
-  $MEDIA_CMD &
+  eval $MEDIA_CMD &
 else
-  # Media player is running → switch back to CarPlay
+  # Media player is running or nothing → switch to CarPlay
   kill_if_running celluloid
   sleep 1
   $CARPLAY_CMD &
