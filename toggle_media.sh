d7e967f5b581d536a3ee93bf76961d9d4af38a28
Shane Williamson <67734801+wert551@users.noreply.github.com>
2025-07-20 00:12:18 -0700

    Update toggle_media.sh

--- toggle_media.sh
+++ toggle_media.sh
@@ -3,20 +3,19 @@
 export DISPLAY=:0
 
 CARPLAY_CMD="/home/pi/react-carplay/start.sh"
-KODI_CMD="kodi --fs"
+MEDIA_DIR="/media/usb"  # update this if your dashcam mounts elsewhere
+VLC_CMD="vlc --fullscreen --qt-start-minimized $MEDIA_DIR"
 
 kill_if_running() {
   pgrep -f "$1" && pkill -f "$1"
 }
 
 if pgrep -f react-carplay > /dev/null; then
-  # CarPlay is running → switch to Kodi
   kill_if_running react-carplay
   sleep 1
-  $KODI_CMD &
+  $VLC_CMD &
 else
-  # Kodi is running or nothing → switch to CarPlay
-  kill_if_running kodi
+  kill_if_running vlc
   sleep 1
   $CARPLAY_CMD &
 fi
