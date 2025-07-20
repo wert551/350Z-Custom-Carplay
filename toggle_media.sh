4b994c27d9d0c771c2d23748899f49391e94a9cd
Shane Williamson <67734801+wert551@users.noreply.github.com>
2025-07-20 01:14:22 -0700

    Update toggle_media.sh

--- toggle_media.sh
+++ toggle_media.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 
 export DISPLAY=:0
-
+DASHCAM_PATH="/media/pi/DASHCAM/DCIM"
 CARPLAY_CMD="/home/pi/react-carplay/start.sh"
-HARUNA_CMD="flatpak run org.kde.haruna /media/pi/DASHCAM"
+
+if [ -d "$DASHCAM_PATH" ]; then
+  HARUNA_CMD="flatpak run org.kde.haruna \"$DASHCAM_PATH\""
+else
+  HARUNA_CMD="flatpak run org.kde.haruna --fullscreen"
+fi
 
 kill_if_running() {
   pgrep -f "$1" && pkill -f "$1"
 }
 
 if pgrep -f react-carplay > /dev/null; then
-  # Switch from CarPlay to Haruna
   kill_if_running react-carplay
   sleep 1
-  $HARUNA_CMD &
+  eval $HARUNA_CMD &
 else
-  # Switch back to CarPlay
   kill_if_running haruna
   sleep 1
   $CARPLAY_CMD &
