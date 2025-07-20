570fd5d4dbd600966c8e8596bb4d16013a7d27ea
Shane Williamson <67734801+wert551@users.noreply.github.com>
2025-07-20 02:25:26 -0700

    Update gpio_toggle.py

--- gpio_toggle.py
+++ gpio_toggle.py
@@ -3,13 +3,11 @@ from gpiozero import Button
 from signal import pause
 import subprocess
 
-# GPIO pin 17 = BCM 17 = physical pin 11
-button = Button(17, pull_up=True)
-
 def on_press():
     print("[GPIO] Button pressed! Running toggle_media.sh")
     subprocess.call(['/usr/local/bin/toggle_media.sh'])
 
+button = Button(17, pull_up=True)
 button.when_pressed = on_press
 
-pause()  # Keep script running
+pause()
